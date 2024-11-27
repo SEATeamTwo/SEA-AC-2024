00ffc39555900ff3bc28cc392647038da486c5ae
sea7 <sea7@ukdfd2dnfq6w.bskyb.com>
2024-11-27 15:13:25 +0000

    submit

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,8 +1,9 @@
 import SkyLogo from "./components/SkyLogo"
 import TitleBar from "./components/TitleBar"
 import Sliders from "./components/Sliders"
-import React from 'react'
+import React, { useState } from 'react'
 import MainContainer from './components/MainContainer'
+import Button from "./components/Button"
 
 function App() {
 
@@ -15,12 +16,23 @@ function App() {
 
   return (
     <MainContainer>
+      <div style={{display: "flex"}}>
+
       <SkyLogo/>
       <TitleBar/>
-      Calm <Sliders value={value1} setValue={setValue1} /> 
-      Cheerful <Sliders value={value2} setValue={setValue2}/> 
-      Awake <Sliders value={value3} setValue={setValue3}/> 
-      Fearless <Sliders value={value4} setValue={setValue4}/> 
+
+      </div>
+
+      <div style={{display: "flex" }}>
+        <Button title={"Use Webcam"}/>
+        <Button title={"Choose File"}/>
+        <Button title={"Submit"} arr={arr}/>
+
+      </div>
+      <Sliders value={value1} setValue={setValue1} /> 
+      <Sliders value={value2} setValue={setValue2}/> 
+      <Sliders value={value3} setValue={setValue3}/> 
+      <Sliders value={value4} setValue={setValue4}/> 
 
     </MainContainer>
 
--- front-end/src/components/Button.jsx
+++ front-end/src/components/Button.jsx
@@ -1,6 +1,16 @@
-export default function Button({title}) {
+export default function Button({title, arr}) {
+
+
+  function handleSubmit() {
+    fetch(`http://localhost:5000/getMovieRecs/${arr}`, {  // Enter your IP address here
+      method: 'POST', 
+    }).then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
+  }
+  
   return (
-    <button>
+    <button onClick={handleSubmit}>
       {title}
     </button>
   )
